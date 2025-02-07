b6016ade54ca0edc3d2c24bb5b261c56ae0061ce
Hammad Yousaf <hammad.yousaf.1929@slashnext.net>
2025-02-07 15:37:32 +0500

    new commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 from reportlab.pdfgen import canvas
 import requests
 
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
